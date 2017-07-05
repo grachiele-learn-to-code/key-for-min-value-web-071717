00ec2165595b10e4d17f4315676e97a2e6e62203
Gianpaul Rachiele <gianpaul817@gmail.com>
2017-07-05 23:24:32 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,25 @@
 
 def key_for_min_value(name_hash)
 
+  keys = []
+
+  name_hash.each_key do |key|
+    keys << key
+  end
+
+  smallest = name_hash[keys[0]]
+
+  smallest_key = nil
+
+  keys.each_index do |idx|
+    if name_hash[keys[idx]] < smallest
+      smallest = name_hash[keys[idx]]
+      smallest_key = keys[idx]
+    elsif name_hash[keys[idx]] > smallest
+      smallest
+    else
+      smallest_key = keys[idx]
+    end
+  end
+  smallest_key
 end
